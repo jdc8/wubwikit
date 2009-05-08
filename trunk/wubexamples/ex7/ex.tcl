b44f09be2acc32c9364b4bb3bf87475bf73a729d
jos.decoster <jos.decoster@774c0e2c-fe1a-11dd-88c7-29a3b14d5316>
2009-05-08 08:34:12 +0000

    added jq ajax example

--- trunk/wubexamples/ex7/ex.tcl
+++ trunk/wubexamples/ex7/ex.tcl
@@ -7,13 +7,31 @@ package require jQ
 namespace eval MyDirectDomain {
     proc /test { req } {
 	set C [<p> [<input> type text id myTimeEntry size 10 {}]]
+	append C [<p> [<input> type text id myTimeEntry2 size 10 {}]]
 	set req [jQ jquery $req]
 	set req [jQ timeentry $req #myTimeEntry]
-	append C [<p> {<button type='button' onclick="$('#myTimeEntry').timeEntry();">Time</button>}]
+	set req [jQ timeentry $req #myTimeEntry2 {show24Hours true showSeconds true}]
 	dict set req -content $C
 	dict set req content-type x-text/html-fragment
 	dict set req -title "MyDirectDomain: test post method"
 	return $req	
+    }
+    proc /test_ajax { req } { 
+	set C [<div> id contents {}]
+	append C "<button type='button' onclick='load_contents();'>Reload</button>"
+	append C [<script> src /scripts/ajax.js]
+	dict set req -content $C
+	dict set req content-type x-text/html-fragment
+	dict set req -title "MyDirectDomain: test post method"
+	set req [jQ jquery $req]
+	set req [jQ ready $req "load_contents();"]
+	return $req	
+    }
+    proc /test_ajax_callback { req } { 
+	set C [<h1> "Time is: [clock format [clock seconds]]"]
+	dict set req -content $C
+	dict set req content-type text/html
+	return $req
     }
     proc /default { req } { 
 	set content "Default function for MyDirectDomain"
@@ -30,7 +48,7 @@ namespace eval MyDirectDomain {
 }
 
 package require conversions
-#set Html::XHTML 1
-#set ::conversions::htmlhead {<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">}
+set Html::XHTML 1
+set ::conversions::htmlhead {<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">}
 
 Site start home . nubs ex.nub ini ex.ini
